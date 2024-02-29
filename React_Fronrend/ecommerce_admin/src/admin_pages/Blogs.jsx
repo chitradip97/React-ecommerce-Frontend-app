c75c746469dc701103bc20f96febcb9e0c021c1b
chitradip97 <chitradip97@gmail.com>
2024-03-01 00:55:27 +0530

    Order page complete. Blog page initiated

--- React_Fronrend/ecommerce_admin/src/admin_pages/Blogs.jsx
+++ React_Fronrend/ecommerce_admin/src/admin_pages/Blogs.jsx
@@ -1,11 +1,96 @@
-import React from 'react'
+import React from 'react';
+import { useState,useEffect,useRef } from "react";
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import JoditEditor from 'jodit-react';
 
 function Blogs() {
+    const login=localStorage.getItem('login');
+    const[loginenable,setLoginenable]=useState('');
+    const editor = useRef(null);
+    const [content, setContent] = useState('');
+    useEffect(()=>{
+    setLoginenable(login)
+    },[loginenable])
+    console.log(login)
+
+    const [catName,setCatname]=useState("");
+    const [catStock,setCatstock]=useState("");
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+       
+            axios.post("http://127.0.0.1:1234/api/category",{catName:catName,catStock:catStock})
+               .then((Response)=>{
+                   console.log(Response.data);
+                   setCatname(" ");
+                   setCatstock(" ");
+                   toast.success("Insert Successfully!",{
+                    position: "top-center",
+                   });
+               })  
+       }
+    if(login){
     return ( 
         <>
-        <h1>this is blogs</h1>
+            <div class="container-fluid">
+                
+                <div className="row">
+                    <div className="col-xl-12 col-md-12">
+                    <div className="card  mb-4 ">
+                        <div className="card-footer"> 
+                        <span id="alert_contain"></span>    
+                        <h5 className="basic_font">Add Blogs</h5> 
+                        </div>
+                        <div className="card-body">
+                        <form >
+                                <div className="mb-3 mt-1">
+                                <label for="category_name" className="form-label basic_font">Blog Title :</label>
+                                <input type="text" className="form-control"  placeholder="Enter your Blog title" onChange={(e)=>{setCatname(e.target.value)}}/>
+                                </div>
+                               
+                                <div className="mb-3 mt-1">
+                                <label for="comment" className="form-label basic_font">Blog category :</label>
+                                <input type="number" className="form-control"  placeholder="Enter your Blog Category " onChange={(e)=>{setCatstock(e.target.value)}}/>
+                                
+                                </div>
+                                <div className="mb-3 mt-1">
+                                <label for="comment" className="form-label basic_font">Write your Blog: :</label>
+                                <JoditEditor 
+                                ref={editor}
+                                value={content}
+                                onChange={newcontent=>setContent(newcontent)}
+                                />
+                                </div>
+                                
+                                <input type="button" className="btn btn-primary"  value="ADD" onClick={handleSubmit}/>
+                            </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <ToastContainer />
         </>
      );
+    }
+    else{
+        return(
+          <>
+            <div className="container mx-4 mt-2 ">
+              <div className="row">
+              <div class="alert alert-danger">
+                <strong>Permission denied!</strong> Please Login first.
+              </div>
+    
+              </div>
+            {/* <h1>Please Login first</h1> */}
+            </div>
+          </>
+    
+        )
+        
+      }
 }
 
 export default Blogs;
--- React_Fronrend/ecommerce_admin/src/admin_pages/Orders.jsx
+++ React_Fronrend/ecommerce_admin/src/admin_pages/Orders.jsx
@@ -1,11 +1,173 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import '../admin_assets/css/Product_style.css';
+import { Outlet } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 function Orders() {
-    return ( 
-        <>
-        <h1>This is Orders</h1>
-        </>
-     );
+    const [orders, setOrders] = useState([]);
+    async function getAllorder() {
+    try {
+        const orders_all = await axios.get(
+          "http://127.0.0.1:1234/api/orders"
+        );
+        console.log(orders_all.data);
+        setOrders(orders_all.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    useEffect(() => {
+      
+        getAllorder();
+    }, []);
+    // const handleDelete=(id)=>{
+    
+    //     axios.delete(`http://127.0.0.1:1234/api/customer-view/${id}`).then(()=>{getAllcustomer()})
+    //    }
+       const login=localStorage.getItem('login');
+       const[loginenable,setLoginenable]=useState('');
+       useEffect(()=>{
+       setLoginenable(login)
+       },[loginenable])
+       console.log(login)
+       if(login)
+       {
+       return ( 
+           // <>
+          
+   
+           //    <div className='product'>
+           // {/* <h1>This is Product</h1> */}
+           // <ul>
+           //     <li><NavLink to='Customer-details'>Customer-details</NavLink></li>
+           //     <li><NavLink to='Customer-purchase'>Customer-purchase</NavLink></li>
+           // </ul>
+           // <div className='prod_box'>
+           // <Outlet/>
+           // </div>
+           
+           // </div>
+           // </>
+           <>
+        {/* <h1>This is customer page</h1> */}
+           <div class="content_section">
+                   <div class="container-fluid">
+                   
+                   <div class="row">
+                       <div class="col-xl-12 col-md-12">
+                       <div class="card  ">
+                           <div class="card-footer"> <h5 class="basic_font">Order List :</h5> </div>
+                           <div class="card-body ">
+                               
+                                              
+                                   <table class="table table-striped">
+                                   <thead>
+                                   <tr>
+                                   <th>Order sr. Id</th>
+                                    <th>Customer Id.</th>
+                                    <th>Tracking no.</th>
+                                    <th>Invoice No</th>
+                                    <th>Total Amount</th>
+                                    <th>Order Date</th>
+                                    <th>Payment Status</th>
+                                    <th>Action</th>
+                                   </tr>
+                                   </thead>
+                                   <tbody>
+   
+                                   {orders.map((order, i) => {
+                                       return (
+                                       <tr key={i}>
+                                           <td>{i+1}</td>
+                                           <td>{order.customer_id}</td>
+                                           <td>{order.tracking_no}</td>
+                                           <td>{order.invoice_no}</td>
+                                           <td>{order.total_amount}</td>
+                                           <td>{order.tracking_no}</td>
+                                           <td>{order.payment_mode}</td>
+                                           <td><button className="btn btn-danger"
+                                            //  onClick={()=>{handleDelete(order.id)}}
+                                            >Delete</button></td>
+                                       </tr>
+                                       // <h2 key={i}>
+                                           // {/* {student.stuname}={student.email} */}
+                                       // </h2>
+                                       );
+                                   })}
+                                   </tbody>
+                                   </table>
+   
+   
+   
+                                    {/* <tr>
+                                       <td>1</td>
+                                       <td>Shusant Khan</td>
+                                       <td>susanta@gmail.com</td>
+                                       <td>7925462360</td>
+                                       <td>Kolkata</td>
+                                       <td>
+                                       <a href="view_customer_edit.php"><button type="button" class="btn btn-success">Edit</button></a>
+                                       <a href="view_customer_delete.php"><button type="button" class="btn btn-danger">Delete</button></a>
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td>2</td>
+                                       <td>Rajib Rawat</td>
+                                       <td>Rajib@gmail.com</td>
+                                       <td>8569354628</td>
+                                       <td>Dumdum</td>
+                                       <td>
+                                       <a href="view_customer_edit.php"><button type="button" class="btn btn-success">Edit</button></a>
+                                       <a href="view_customer_delete.php"><button type="button" class="btn btn-danger">Delete</button></a>
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td>3</td>
+                                       <td>Aman Guha</td>
+                                       <td>aman@gmail.com</td>
+                                       <td>7965354985</td>
+                                       <td>Siliguri</td>
+                                       <td>
+                                       <a href="view_customer_edit.php"><button type="button" class="btn btn-success">Edit</button></a>
+                                       <a href="view_customer_delete.php"><button type="button" class="btn btn-danger">Delete</button></a>
+                                       </td>
+                                   </tr>
+                                  
+                                   </tbody>
+                                   </table> */}
+                               
+                               
+                           </div>
+                       </div>
+                       </div>
+                       
+                   </div>
+                   </div>
+                   
+                  
+              </div>
+       </>
+        );
+       }
+       else{
+           return(
+             <>
+               <div className="container mx-4 mt-2 ">
+                 <div className="row">
+                 <div class="alert alert-danger">
+                   <strong>Permission denied!</strong> Please Login first.
+                 </div>
+       
+                 </div>
+               {/* <h1>Please Login first</h1> */}
+               </div>
+             </>
+       
+           )
+           
+         }
 }
 
 export default Orders;
